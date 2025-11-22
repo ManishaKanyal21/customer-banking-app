11cf1d575ceb9662264429674977b2aafcd03925
ManishaKanyal21 <manishajobs21@gmail.com>
2025-11-22 15:15:56 +0000

    Initial commit

---
+++ src/main/java/com/pismo/banking/CustomerBankingApplication.java
@@ -0,0 +1,13 @@
+package com.pismo.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerBankingApplication.class, args);
+	}
+
+}
